4113ef96571dd3884183b0581706b182f12244c8
ahanam05 <ahanam05@gmail.com>
2024-07-02 08:12:09 +0530

    Created Author Model

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-if(process.env.NODE_ENV !== 'production'){
+const { connect } = require("http2");
+require('dotenv').config();
+
+/*if(process.env.NODE_ENV !== 'production'){
     require('dotenv').config();
-}
+} -- SHOULD BE UNCOMMMENTED BUT??*/
 
 const express = require("express");
 const app = express();
@@ -16,10 +19,26 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL); //figures out where to host based on env
+const connectionString = process.env.DATABASE_URL;
+
+//connecting to mongoDb atlas 
+
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(connectionString, {
+            autoIndex: true
+        })
+        console.log('Connected to Mongodb Atlas');
+    } catch (error) {
+        console.error(error);
+    }
+}
+connectToDB();
+
+/*mongoose.connect(process.env.DATABASE_URL); //figures out where to host based on env
 const db = mongoose.connection;
 db.on('error', error => console.error(error)); //displays error if any
-db.on('open', () => console.log('Connected to Mongoose')); //confirms connection to mongoose
+db.on('open', () => console.log('Connected to Mongoose')); //confirms connection to mongoose*/
 
 app.use('/', indexRouter); //mounted at the very root of the site
 app.use('/authors', authorRouter);
